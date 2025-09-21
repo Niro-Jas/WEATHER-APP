5bbf77dec7d6869e8edb0bf140614244b0890f53
Niranjana <aspirantniraan26@gmail.com>
2025-09-21 22:05:20 +0530

    Update Weather.jsx

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -63,40 +63,41 @@ const Weather = () => {
   }
 
   return (
-    <div className="weather">
-      <div className="search-bar">
-        <input ref={inputRef} type="text" placeholder="Search city..." />
-        <img
-          src={search_icon}
-          alt="Search"
-          onClick={() => search(inputRef.current.value)}
-          style={{ cursor: "pointer" }}
-        />
-      </div>
+    <div className="weather-container">
+      <div className="weather">
+        <div className="search-bar">
+          <input ref={inputRef} type="text" placeholder="Search city..." />
+          <img
+            src={search_icon}
+            alt="Search"
+            onClick={() => search(inputRef.current.value)}
+          />
+        </div>
 
-      {weatherData && (
-        <>
-          <img src={weatherData.icon} alt="" className="weather-icon" />
-          <p className="temperature">{weatherData.temperature}°C</p>
-          <p className="location">{weatherData.location}</p>
-          <div className="weather-data">
-            <div className="col">
-              <img src={humidity_icon} alt="" />
-              <div>
-                <p>{weatherData.humidity}%</p>
-                <span>Humidity</span>
+        {weatherData && (
+          <>
+            <img src={weatherData.icon} alt="" className="weather-icon" />
+            <p className="temperature">{weatherData.temperature}°C</p>
+            <p className="location">{weatherData.location}</p>
+            <div className="weather-data">
+              <div className="col">
+                <img src={humidity_icon} alt="" />
+                <div>
+                  <p>{weatherData.humidity}%</p>
+                  <span>Humidity</span>
+                </div>
+              </div>
+              <div className="col">
+                <img src={wind_icon} alt="" />
+                <div>
+                  <p>{weatherData.windSpeed} Km/h</p>
+                  <span>Wind</span>
+                </div>
               </div>
             </div>
-            <div className="col">
-              <img src={wind_icon} alt="" />
-              <div>
-                <p>{weatherData.windSpeed} Km/h</p>
-                <span>Wind</span>
-              </div>
-            </div>
-          </div>
-        </>
-      )}
+          </>
+        )}
+      </div>
     </div>
   )
 }
